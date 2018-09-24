e0a5c7f27cf4e844f0b7823422bb9545c939fd25
POA-INFO Jairo Conceicao <Jairo.POA@penagro.mocrusul>
2018-09-24 18:03:41 -0300

    Tela de Edição

--- web/src/components/ClientesNew.vue
+++ web/src/components/ClientesNew.vue
@@ -2,12 +2,23 @@
   <section class="container">
     <h1>Novo</h1>
     <div class="row">
-      <div v-for="ctrl in formElements" :key="ctrl['label']" :class="ctrl['layout']">
+      <div v-for="(ctrl, index) in formElements" :key="index" :class="ctrl['layout']">
         <label>{{ctrl['label']}}</label>
-        <input v-if="ctrl['type'] === 'text'" type="text" class="form-control" />
-        <input v-else-if="ctrl['type'] === 'check'" type="check" class="form-control" />
-        <input v-else-if="ctrl['type'] === 'radio'" type="radio" class="form-control" />
-        <textarea v-else-if="ctrl['type'] === 'textarea'" class="form-control"></textarea>
+        <input v-if="ctrl['type'] === 'text'" type="text" class="form-control" v-model="dataModel[index]" />
+
+        <input v-else-if="ctrl['type'] === 'check'" type="checkbox" class="form-control" v-model="dataModel[index]" />
+
+        <div v-else-if="ctrl['type'] === 'radio'">
+          <div v-for="item in ctrl['enum']" :key="item" class="form-check">
+            <input type="radio" class="form-check-input" v-model="dataModel[index]" />
+            <label class="form-check-label">{{item}}</label>
+          </div>
+        </div>
+
+        <textarea v-else-if="ctrl['type'] === 'textarea'" class="form-control" v-model="dataModel[index]"></textarea>
+        <select v-else-if="ctrl['type'] === 'select'" class="form-control" v-model="dataModel[index]">
+          <option v-for="option in ctrl['enum']" :value="option" :key="option"> {{option}} </option>
+        </select>
       </div>
     </div>
     <div class="row">
